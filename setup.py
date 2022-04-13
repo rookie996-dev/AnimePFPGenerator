0099e60e1c966df6360b79e620f97fc1a4183570
LUA9 <99425047+LUA9@users.noreply.github.com>
2022-04-13 18:55:23 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import re
+from setuptools import setup
+
+with open('AnimePFP.py', 'r', encoding='utf-8') as file:
+    code = file.read()
+
+file.close()
+
+version_code = re.search(r'__version__ = .*', code).group()
+exec(version_code, globals(), locals())
+
+setup(
+    name='AnimePFP',
+    version=__version__, # type: ignore
+    description='Anime profile picture downloader using https://avatars.alphacoders.com/by_category/3',
+    author='LUA9',
+    maintainer='LUA9',
+    url='https://github.com/LUA9/AnimePFPGenerator',
+    py_modules=['AnimePFP'],
+    requires=['aiohttp', 'pyppeteer', 'bs4'],
+    long_description='# Example\n```python\nimport AnimePFP\nAnimePFP.GetAnimePictures(directory=\'test\')```',
+    long_description_content_type='text/markdown',
+)
