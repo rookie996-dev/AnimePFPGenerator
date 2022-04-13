37b873e020217876cf4b5b5b33aafa384b4a7b86
LUA9 <99425047+LUA9@users.noreply.github.com>
2022-04-14 00:00:19 +0900

    Update AnimePFP.py

--- AnimePFP.py
+++ AnimePFP.py
@@ -1,31 +1,30 @@
+from __future__ import annotations
+
 import aiohttp
 import secrets
 import asyncio
 import pyppeteer
 
 from os import PathLike
+from os.path import join
 from bs4 import BeautifulSoup
-from typing import Union, Optional
 
 __all__ = ('AsyncGetAnimePictures', 'GetAnimePictures')
-__version__ = '1.0.0'
+__version__ = '1.0.2a'
 
-BACKSLASH = '\\'
-
-async def AsyncGetAnimePictures(directory: Optional[Union[str, PathLike[str]]] = None, format: Optional[Union[str, bytes]] = None, stop: Optional[int] = None, *, headless: Optional[bool] = None, delay: Optional[int] = None, session: Optional[aiohttp.ClientSession] = None, semaphore: Optional[asyncio.Semaphore] = None):
-    format = format or 'png'
-    if isinstance(format, bytes):
-        format = format.decode()
-    if format.startswith('.'):
-        format = format[1:]
-    
-    stop = stop or 10
-    browser = await pyppeteer.launch(headless=headless if headless is not None else True)
+async def AsyncGetAnimePictures(
+    directory: str | PathLike[str] | None = '', 
+	stop: int | None = 10, 
+	*, 
+	headless: bool | None = True, 
+	delay: int | None = 500, 
+):    
+    browser = await pyppeteer.launch(headless=headless)
     page = await browser.newPage()
     await page.goto('https://avatars.alphacoders.com/by_category/3')
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36 WAIT_UNTIL=load')
 
-    for _ in range(stop):
+    async def evaluate():
         await page.evaluate(
             '''
             async () => {
@@ -38,21 +37,30 @@ async def AsyncGetAnimePictures(directory: Optional[Union[str, PathLike[str]]] =
             }
             '''
         )
-        await page.waitFor(delay or 500)
+        await page.waitFor(delay)
+        
+    map(lambda _: (await evaluate() for _ in '_').__anext__(), range(stop))
 
     soup = BeautifulSoup(await page.content(), 'html.parser')
     images = [image.get('src') for image in soup.findAll('img')]
 
-    async with semaphore or asyncio.Semaphore(20 * 10000000000):
-        async with session or aiohttp.ClientSession() as request:
+    async with asyncio.Semaphore(200000000):
+        async with aiohttp.ClientSession() as request:
             for image in images:
+                filename = secrets.token_hex(12)
                 response = await request.get(image, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'})
-                with open(f'{directory + BACKSLASH if directory else ""}{secrets.token_hex(12)}.{format}', 'wb') as file:
+                with open(join(directory, '{}.png'.format(filename)), 'wb') as file:
                     file.write(await response.read())
                 file.close()
 
     return await request.close()
 
-def GetAnimePictures(loop: Optional[asyncio.AbstractEventLoop] = None, directory: Optional[Union[str, PathLike[str]]] = None, format: Optional[Union[str, bytes]] = None, stop: Optional[int] = None, *, headless: Optional[bool] = None, delay: Optional[int] = None, session: Optional[aiohttp.ClientSession] = None, semaphore: Optional[asyncio.Semaphore] = None):
-    loop = loop or asyncio.new_event_loop()
-    return loop.run_until_complete(AsyncGetAnimePictures(directory, format, stop, headless=headless, delay=delay, session=session, semaphore=semaphore))
+def GetAnimePictures(
+    directory: str | PathLike[str] | None = '', 
+	stop: int | None = 10, 
+	*, 
+	headless: bool | None = True, 
+	delay: int | None = 500,
+):    
+    loop = asyncio.new_event_loop()
+    return loop.run_until_complete(AsyncGetAnimePictures(directory, stop, headless=headless, delay=delay))
